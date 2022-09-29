9e030ff4952b1f6157310f97e90a6faebc67f072
Felix Orinda <55891685+forinda@users.noreply.github.com>
2022-09-29 16:35:23 +0300

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from .view import base as baseBlueprint
+from .config import config
 
 
 def create_app():
 	app = Flask(__name__)
+	app.config.from_object(config['dev'])
 	
 	app.register_blueprint(baseBlueprint)
 	return app
